bf016f638f8a3d9599044b290a76a97572076195
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-02-21 23:41:17 -0300

    feat: created first page component, home

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/generic/headerComponent";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
