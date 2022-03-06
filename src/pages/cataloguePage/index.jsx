231077d54247f5741efb1ba841a82b60a41f95e9
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-05 22:15:49 -0300

    feat: created  species list page

---
+++ src/pages/cataloguePage/index.jsx
@@ -0,0 +1,14 @@
+const CataloguePage = () => {
+  return (
+    <div>
+      <h1>Hello! I'm **a list** of snekes</h1>
+      <ul>
+        <li>
+          <button>Primeira especie</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default CataloguePage;
