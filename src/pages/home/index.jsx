e12c8a455f576b742a7d1b92076ce40980e731a5
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-05 21:49:52 -0300

    feat: added Global style to home page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/generic/headerComponent";
+import GlobalStyle from "../../styles/globalStyles";
 
 const HomePage = () => {
   return (
     <>
-      <Header />
+      <GlobalStyle/>
+      <Header />      
       <main>
         <h1>Home</h1>
       </main>
