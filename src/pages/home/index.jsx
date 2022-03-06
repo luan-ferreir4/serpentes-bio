cf0a130779e2f9cd82992a2c96c01c7563447561
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-05 22:17:15 -0300

    refactor: importing GlobalStyles on App

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,8 @@
 import Header from "../../components/generic/headerComponent";
-import GlobalStyle from "../../styles/globalStyles";
 
 const HomePage = () => {
   return (
     <>
-      <GlobalStyle/>
       <Header />      
       <main>
         <h1>Home</h1>
